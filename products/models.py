f1e9f19a44658f8dc431eb31deb54e90cef44809
ali <ali.rasoulinejad58@gmail.com>
2019-07-30 21:06:54 -0700

    create model 'Categories'

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+    parent_category = models.ForeignKey("self", on_delete=models.CASCADE)
+
