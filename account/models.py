af27288fd581af091da85d76365d97e1230195da
ali <ali.rasoulinejad58@gmail.com>
2019-08-23 19:43:29 -0700

    Update model City

--- account/models.py
+++ account/models.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        default_related_name = 'city'
+        verbose_name = 'city'
+        verbose_name_plural = 'cities'
+        db_table = 'user_city'
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Zone(models.Model):
